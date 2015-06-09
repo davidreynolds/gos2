784db26b4d3036f1f98a5de5d5c0f99d7dbf379c
Daniel Cheng <sdiz@sdiz.net>
2015-06-09 11:14:26 +0800

    Implements Region for Polyline

--- s2/polyline.go
+++ s2/polyline.go
@@ -17,3 +17,49 @@ func (pl *Polyline) initPoints(vertices []Point) {
 
 func (pl Polyline) NumVertices() int   { return len(pl.vertices) }
 func (pl Polyline) Vertex(k int) Point { return pl.vertices[k] }
+
+func (pl *Polyline) GetRectBound() *Rect {
+	rb := NewRectBounder()
+	for _, pt := range pl.vertices {
+		rb.AddPoint(&pt)
+	}
+	bound := rb.Bound()
+	return &bound
+}
+
+func (pl *Polyline) MayIntersect(cell Cell) bool {
+	if pl.NumVertices() == 0 {
+		return false
+	}
+
+	for _, pt := range pl.vertices {
+		if cell.ContainsPoint(pt) {
+			return true
+		}
+	}
+
+	cellVertices := []Point{
+		cell.Vertex(0),
+		cell.Vertex(1),
+		cell.Vertex(2),
+		cell.Vertex(3),
+	}
+
+	for j := 0; j < 4; j++ {
+		crosser := NewEdgeCrosser(&cellVertices[j], &cellVertices[(j+1)&3], &pl.vertices[0])
+		for _, vetex := range pl.vertices[1:] {
+			if crosser.RobustCrossing(&vetex) >= 0 {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func (pl *Polyline) ContainsCell(cell Cell) bool { return false }
+func (pl *Polyline) ContainsPoint(p Point) bool  { return false }
+
+func (pl *Polyline) CapBound() Cap {
+	return pl.GetRectBound().CapBound()
+}
